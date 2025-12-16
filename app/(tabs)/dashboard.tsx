ed9c6cba8a957b4f6e30a5866f84a20fb19bc016
shlokshrestha <shlokstha99@gmail.com>
2025-12-16 12:32:45 +0545

    added apollo client

--- app/(tabs)/dashboard.tsx
+++ app/(tabs)/dashboard.tsx
@@ -10,6 +10,7 @@ import {
   Image,
   RefreshControl,
   ActivityIndicator,
+  ScrollView,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -44,52 +45,57 @@ const Dashboard = () => {
     );
   }
   return (
-    <SafeAreaView>
-      <View
-        style={{
-          alignItems: "center",
-          padding: 10,
-        }}
-      >
-        <Text style={{ fontSize: 24, fontWeight: "600" }}>Products List</Text>
-      </View>
-      <FlatList
-        data={data.products}
-        keyExtractor={(item) => item.id.toString()}
-        contentContainerStyle={{ padding: 8 }}
-        refreshControl={
-          <RefreshControl refreshing={isLoading} onRefresh={refetch} />
-        }
-        renderItem={({ item }) => (
-          <View
-            style={{
-              marginBottom: 20,
-              padding: 15,
-              backgroundColor: "#f2f2f2",
-              borderRadius: 10,
-            }}
-          >
-            {item.thumbnail && (
-              <Image
-                source={{ uri: item.thumbnail }}
-                style={{
-                  width: "100%",
-                  height: 200,
-                  borderRadius: 8,
-                  marginBottom: 10,
-                }}
-                resizeMode="cover"
+    <ScrollView>
+      <SafeAreaView>
+        <View
+          style={{
+            alignItems: "center",
+            padding: 10,
+          }}
+        >
+          <Text style={{ fontSize: 24, fontWeight: "600" }}>Products List</Text>
+        </View>
+        <FlatList
+          data={data.products}
+          keyExtractor={(item) => item.id.toString()}
+          contentContainerStyle={{ padding: 8 }}
+          refreshControl={
+            <RefreshControl refreshing={isLoading} onRefresh={refetch} />
+          }
+          renderItem={({ item }) => (
+            <View
+              style={{
+                marginBottom: 20,
+                padding: 15,
+                backgroundColor: "#f2f2f2",
+                borderRadius: 10,
+              }}
+            >
+              {item.thumbnail && (
+                <Image
+                  source={{ uri: item.thumbnail }}
+                  style={{
+                    width: "100%",
+                    height: 200,
+                    borderRadius: 8,
+                    marginBottom: 10,
+                  }}
+                  resizeMode="cover"
+                />
+              )}
+              <Text style={{ fontSize: 18, fontWeight: "600" }}>
+                {item.title}
+              </Text>
+              <Text style={{ marginBottom: 10 }}>Price: {item.price}</Text>
+              <Button
+                title="View Details"
+                onPress={() => goToProduct(item.id)}
               />
-            )}
-            <Text style={{ fontSize: 18, fontWeight: "600" }}>
-              {item.title}
-            </Text>
-            <Text style={{ marginBottom: 10 }}>Price: {item.price}</Text>
-            <Button title="View Details" onPress={() => goToProduct(item.id)} />
-          </View>
-        )}
-      />
-    </SafeAreaView>
+            </View>
+          )}
+        />
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,26 @@
 import { Stack } from "expo-router";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "./global.css";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
 export const queryClient = new QueryClient();
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://graphqlplaceholder.vercel.app/graphql",
+  }),
+  cache: new InMemoryCache(),
+});
+
 export default function RootLayout() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <SafeAreaProvider>
-        <Stack screenOptions={{ headerShown: false }} />
-      </SafeAreaProvider>
-    </QueryClientProvider>
+    <ApolloProvider client={client}>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaProvider>
+          <Stack screenOptions={{ headerShown: false }} />
+        </SafeAreaProvider>
+      </QueryClientProvider>
+    </ApolloProvider>
   );
 }
